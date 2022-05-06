9ba35106daf135691576f67226f16b1f1f3e30a7
lucian <lucian@humanscape.io>
2022-05-06 18:17:27 +0900

    study

---
+++ src/cat/cat.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Put, Delete, Req, HttpCode } from '@nestjs/common';
+import { Request } from 'express';
+
+@Controller('cats')
+export class CatController {
+  @Get()
+  findAll(@Req() request: Request): object {
+    return request.query
+  }
+  @Post()
+  // @HttpCode(201)
+  create(@Req() request: Request): object {
+    return request.body
+  }
+  @Get(':id')
+  findOne(@Req() request: Request): object {
+    return request.params;
+  }
+  @Put(':id')
+  update(@Req() request: Request): object {
+    const resp = {
+      id: request.params.id,
+      body: request.body
+    }
+    return resp;
+  }
+  @Delete(':id')
+  @HttpCode(204)
+  destroy(@Req() request: Request): object {
+    return request.params;
+  }
+}
